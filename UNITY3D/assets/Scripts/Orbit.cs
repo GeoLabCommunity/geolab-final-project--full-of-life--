465f114b13ebd865a2a50931d77d7cc25e334c3d
kavla <giorgi.kavlashvili01@geolab.edu.ge>
2016-01-17 19:18:23 +0400

    Unity3D Orbit

---
+++ UNITY3D/assets/Scripts/Orbit.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit : MonoBehaviour {
+	float timeCounter=0;
+
+	float speed;
+	float l1;
+	float l2;
+
+	// Use this for initialization
+	void Start () {
+		speed = 1;
+		l1 = 1;
+		l2 = 1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timeCounter += Time.deltaTime*speed;
+
+		float x = Mathf.Cos (timeCounter)*l1;
+		float y = 0;
+		float z = Mathf.Sin (timeCounter)*l2;
+
+		transform.position = new Vector3 (x, y, z);
+	}
+}
